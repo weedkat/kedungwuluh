44f679f0ad7a3c5988bbeb9fb110297ab02f73e5
weedkat <fatahmurbaut@gmail.com>
2022-02-06 08:12:43 +0700

    page narkoba

--- resources/views/blog/narkoba.blade.php
+++ resources/views/blog/narkoba.blade.php
@@ -13,7 +13,7 @@
                     <ol>
                         <li><a href="index.html">Home</a></li>
                         <li><a href="blog.html">Blog</a></li>
-                        <li>Dolorum optio tempore voluptas dignissimos cumque fuga qui quibusdam quia reiciendis</li>
+                        <li>Narkoba</li>
                     </ol>
                 </div>
 
@@ -31,115 +31,113 @@
                             <div class="container">
 
                                 <div class="section-title">
-                                    <h2>Features</h2>
-                                    <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum
-                                        quidem. Sit sint
-                                        consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit
-                                        alias ea. Quia
-                                        fugiat
-                                        sit in iste officiis commodi quidem hic quas.</p>
+                                    <h2>Kenali Depresi Pada Anak Anda Agar Terhindar Dari Narkoba</h2>
+                                    <p>Depresi pada anak sering kali tidak disadari orang tua. Kenali gejala depresi pada
+                                        anak serta pencegahan dan pengobatan yang diperlukan pada ulasan berikut! Umumnya
+                                        depresi terjadi pada orang dewasa, namun anak-anak dan remaja juga bisa terkena
+                                        depresi.
+                                        Jika seorang anak
+                                        tampak sedih dan kesedihan itu terus berlanjut sehingga mengganggu aktivitas sosial,
+                                        tugas sekolah, dan kehidupan keluarga, itu menunjukkan bahwa anak tersebut menderita
+                                        depresi.
+                                    </p>
                                 </div>
 
-                                <div class="row" data-aos="fade-up">
+                                <div class="row d-flex flex-wrap align-items-center" data-aos="fade-up">
                                     <div class="col-md-5">
-                                        <img src="{{ url('/moderna/assets/img/features-1.svg') }}" class="img-fluid"
+                                        <img src="https://images.bisnis-cdn.com/thumb/posts/2021/06/02/1400341/anak-depresi.jpg?w=744&h=465" class="img-fluid "
                                             alt="">
                                     </div>
                                     <div class="col-md-7 pt-4">
-                                        <h3>Voluptatem dignissimos provident quasi corporis voluptates sit assumenda.</h3>
-                                        <p class="fst-italic">
-                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                                            incididunt ut
-                                            labore
-                                            et dolore
-                                            magna aliqua.
+                                        <h3>Tanda Gejala Depresi pada Anak</h3>
+                                        <p>
+                                            Biasanya gejala depresi tidak disadari karena dianggap sebagai perubahan
+                                            emosional dan psikologis yang normal. Gejala-gejala
+                                            yang terjadi saat anak depresi antara lain :
                                         </p>
                                         <ul>
-                                            <li><i class="bi bi-check"></i> Ullamco laboris nisi ut aliquip ex ea commodo
-                                                consequat.</li>
-                                            <li><i class="bi bi-check"></i> Duis aute irure dolor in reprehenderit in
-                                                voluptate velit.
+                                            <li><i class="bi bi-check"></i> Mudah marah</li>
+                                            <li><i class="bi bi-check"></i> Perasaan sedih dan putus asa</li>
+                                            <li><i class="bi bi-check"></i> Menarik diri</li>
+                                            <li><i class="bi bi-check"></i> Kehilangan minat dan ketertarikan pada
+                                                aktivitas yang disukai</li>
+                                            <li><i class="bi bi-check"></i> Nafsu makan bisa meningkat atau justru
+                                                menurun</li>
+                                            <li><i class="bi bi-check"></i> Sulit berkonsentrasi atau pikiran mudah
+                                                terganggu</li>
+                                            <li><i class="bi bi-check"></i> Mudah lelah / kurang energi</li>
+                                            <li><i class="bi bi-check"></i> Perasaan mudah bersalah dan tidak berharga
                                             </li>
+                                            <li><i class="bi bi-check"></i> Keluhan fisik, seperti sakit kepala atau
+                                                sakit perut</li>
                                         </ul>
                                     </div>
                                 </div>
 
-                                <div class="row" data-aos="fade-up">
+                                <div class="row d-flex flex-wrap align-items-center" data-aos="fade-up">
                                     <div class="col-md-5 order-1 order-md-2">
-                                        <img src="{{ url('/moderna/assets/img/features-2.svg') }}" class="img-fluid"
-                                            alt="">
+                                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWDvmcr7QiHrLhxPCBlgt9R-G2aH5MBOsuHg&usqp=CAU" class="img-fluid"
+                                            alt="" width="700">
                                     </div>
-                                    <div class="col-md-7 pt-5 order-2 order-md-1">
-                                        <h3>Corporis temporibus maiores provident</h3>
-                                        <p class="fst-italic">
-                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                                            incididunt ut
-                                            labore
-                                            et dolore
-                                            magna aliqua.
-                                        </p>
+                                    <div class="col-md-7 order-2 order-md-1">
+                                        <h3>Penyebab Depresi Anak</h3>
                                         <p>
-                                            Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                                            in reprehenderit
-                                            in
-                                            voluptate
-                                            velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                                            cupidatat non
-                                            proident,
-                                            sunt in
-                                            culpa qui officia deserunt mollit anim id est laborum
+                                            Faktor depresi anak bermacam-macam, sehingga setiap anak memiliki penyebab
+                                            depresi yang
+                                            berbeda-beda. Depresi pada anak
+                                            dapat disebabkan dari lingkungan sekolah seperti bullying, masalah dengan teman
+                                            atau guru, atau mungkin beban pelajaran yang terlalu berat. Bagi anak remaja,
+                                            depresi bisa disebabkan dari lingkungan sosial yang toksik dan beban
+                                            kehidupan.
+                                        </p>
+
+                                        <p>
+                                            Faktor lain yang juga dapat menjadi penyebab depresi pada anak adalah :
+                                            kesehatan fisik, masalah di kehidupan, riwayat keluarga, dan genetik (bipolar,
+                                            ADHD, OCD).
                                         </p>
                                     </div>
                                 </div>
 
-                                <div class="row" data-aos="fade-up">
+                                <div class="row d-flex flex-wrap align-items-center" data-aos="fade-up">
                                     <div class="col-md-5">
-                                        <img src="{{ url('/moderna/assets/img/features-3.svg') }}" class="img-fluid"
+                                        <img src="https://asset.kompas.com/crops/1U3LriuqIUVUeCfrQNunD79v4Dw=/64x0:649x390/750x500/data/photo/2015/05/07/1414575shutterstock-190160534780x390.jpg" class="img-fluid"
                                             alt="">
                                     </div>
-                                    <div class="col-md-7 pt-5">
-                                        <h3>Sunt consequatur ad ut est nulla consectetur reiciendis animi voluptas</h3>
-                                        <p>Cupiditate placeat cupiditate placeat est ipsam culpa. Delectus quia minima quod.
-                                            Sunt saepe odit
-                                            aut
-                                            quia voluptatem hic voluptas dolor doloremque.</p>
-                                        <ul>
-                                            <li><i class="bi bi-check"></i> Ullamco laboris nisi ut aliquip ex ea commodo
-                                                consequat.</li>
-                                            <li><i class="bi bi-check"></i> Duis aute irure dolor in reprehenderit in
-                                                voluptate velit.
-                                            </li>
-                                            <li><i class="bi bi-check"></i> Facilis ut et voluptatem aperiam. Autem
-                                                soluta ad fugiat.
-                                            </li>
-                                        </ul>
+                                    <div class="col-md-7">
+                                        <h3>Depresi Dapat Memicu Penggunaan Narkoba</h3>
+                                        <p>Tak sedikit orang yang melepaskan stres mereka dengan merokok, minum alkohol,
+                                            atau makan berlebihan, biasanya tergantung dari
+                                            peristiwa atau keadaan yang sedang dihadapi. Depresi menyebabkan orang ingin
+                                            lari dari kehidupan nyata, sehingga
+                                            dapat meningkatkan risiko
+                                            terhadap kecanduan obat-obatan terlarang.
+                                        </p>
+                                        <p>
+                                            Maka dari itu, penting bagi orang tua
+                                            memahami soal deperesi pada anak dan cara menanganinya. Jangan sampai anak anda
+                                            terjerumus dalam lingkaran narkoba karena depresi.
+                                        </p>
                                     </div>
                                 </div>
 
-                                <div class="row" data-aos="fade-up">
+                                <div class="row d-flex flex-wrap align-items-center" data-aos="fade-up">
                                     <div class="col-md-5 order-1 order-md-2">
-                                        <img src="{{ url('/moderna/assets/img/features-4.svg') }}" class="img-fluid"
+                                        <img src="https://image-cdn.medkomtek.com/RHPhv7e81LpjbcfLXSRjl8oSQNI=/673x379/smart/klikdokter-media-buckets/medias/1837019/original/064316100_1516285407-Orang-Tua-Bisa-Menjadi-Penyebab-Depresi-pada-Anak-By-Gladskikh-Tatiana-shutterstock.jpg" class="img-fluid"
                                             alt="">
                                     </div>
                                     <div class="col-md-7 pt-5 order-2 order-md-1">
-                                        <h3>Quas et necessitatibus eaque impedit ipsum animi consequatur incidunt in</h3>
-                                        <p class="fst-italic">
-                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                                            incididunt ut
-                                            labore
-                                            et dolore
-                                            magna aliqua.
+                                        <h3>Cara Mencegah dan Mengatasi Depresi Anak</h3>
+                                        <p>
+                                            Pertolongan pertama adalah berbicara dengan anak secara terbuka dan
+                                            menyemangatinya. Biarkan anak anda mengutarakan keluh kesah isi hatinya.
+                                            Meskipun terdengar sepele, pastikan anak merasa nyaman dan didengar. Kadang obat
+                                            depresi adalah rasa peduli dan kasih sayang dari orang yang ada didekatnya.
                                         </p>
                                         <p>
-                                            Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                                            in reprehenderit
-                                            in
-                                            voluptate
-                                            velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                                            cupidatat non
-                                            proident,
-                                            sunt in
-                                            culpa qui officia deserunt mollit anim id est laborum
+                                            Jika pertolongan pertama tidak berhasil dan anak masih terlihat depresi, ada
+                                            baiknya anda menjadwalkan kunjungan ke dokter untuk dapat didiagnosis dan
+                                            menerima terapi yang tepat.
                                         </p>
                                     </div>
                                 </div>
